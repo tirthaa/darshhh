c92c7d9ef8765e42c35998a79c604dc0ec6a93ec
tirthaa <tirtha@leeoqa.com>
2025-01-15 05:32:03 +0000

    READ_AUto 1 from Server Darshh

--- leeoqa/check/push.sh
+++ leeoqa/check/push.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cd /home/u787416989/domains/leeoqa.com/public_html/TestEnv
-git add TestEnv/leeoqa/QA/*.xls
+git add .
 git config --global user.email "tirtha@leeoqa.com"
 git config --global user.name "tirthaa"
 git commit -m "READ_AUto_2 from Server Darshh" TestEnv/leeoqa/*.xls
