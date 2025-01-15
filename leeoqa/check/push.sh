da62e450e8086fbb00358fce54bfd7ddb2670aad
tirthaa <tirtha@leeoqa.com>
2025-01-15 13:13:03 +0000

    READ_AUto 1 from Server Darshh

--- leeoqa/check/push.sh
+++ leeoqa/check/push.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd /home/u787416989/domains/leeoqa.com/public_html/TestEnv
-git add -A
+git add -A TestEnv/leeoqa/QA/*.xls
 git config --global user.email "tirtha@leeoqa.com"
 git config --global user.name "tirthaa"
-git commit -a "READ_AUto_2 from Server Darshh"
+git commit -a -m "READ_AUto_2 from Server Darshh" TestEnv/leeoqa/QA/*.xls
 git remote add origin https://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@github.com/tirthaa/darshhh.git
 git remote set-url origin https://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@github.com/tirthaa/darshhh.git
 git config --global  pull.ff true
